eb6f2cfdaf8a7f6c8a3e3059ceae429befa64db7
thechampp <kaifj99@gmail.com>
2018-07-14 17:20:56 +0530

    Added mustache templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,23 +10,27 @@ socket.on('disconnect', function(){
 
 socket.on('newMessage',function(message){
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  console.log('New Message arrived ',message);
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template,{
+    from:message.from,
+    text:message.text,
+    createdAt:formattedTime
+  });
 
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
 
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  var template = jQuery('#location-template').html();
+  var html = Mustache.render(template,{
+    from:message.from,
+    url:message.url,
+    createdAt:formattedTime
+  });
+
+  jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit',function(e){
@@ -56,7 +60,7 @@ locationButton.on('click',function(){
       latitude:position.coords.latitude,
       longitude:position.coords.longitude
     },function(){
-
+      locationButton.removeAttr('disabled').text('Send Location');
     });
   },function(){
     locationButton.removeAttr('disabled').text('Send Location');
